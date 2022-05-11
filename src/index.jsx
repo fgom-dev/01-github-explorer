2e8fd84985b09474fec4c8fb682e11dbce6a23d0
fgom-dev <fnd.gomes02@gmail.com>
2022-05-11 16:49:19 -0300

    primeiro commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { App } from './app'
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App/>);
